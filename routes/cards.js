86e0703b423473e45f3b1521dcb1c22638745fc0
julieg18 <juliannagal18@gmail.com>
2020-09-08 05:43:06 -0500

    fix: add catch blocks to fsPromises

--- routes/cards.js
+++ routes/cards.js
@@ -6,9 +6,15 @@ const cardsPath = path.join(__dirname, '..', 'data', 'cards.json');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  fsPromises.readFile(cardsPath, { encoding: 'utf8' }).then((cards) => {
-    res.send(JSON.parse(cards));
-  });
+  fsPromises
+    .readFile(cardsPath, { encoding: 'utf8' })
+    .then((cards) => {
+      res.send(JSON.parse(cards));
+    })
+    .catch(() => {
+      res.status(404);
+      res.send({ message: 'Requested resource not found' });
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,23 +7,35 @@ const usersPath = path.join(__dirname, '..', 'data', 'users.json');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  fsPromises.readFile(usersPath, { encoding: 'utf8' }).then((users) => {
-    res.send(JSON.parse(users));
-  });
+  fsPromises
+    .readFile(usersPath, { encoding: 'utf8' })
+    .then((users) => {
+      res.send(JSON.parse(users));
+    })
+    .catch(() => {
+      res.status(404);
+      res.send({ message: 'Requested resource not found' });
+    });
 });
 
 router.get('/:userId', (req, res) => {
   const { userId } = req.params;
-  fsPromises.readFile(usersPath, { encoding: 'utf8' }).then((users) => {
-    const usersJson = JSON.parse(users);
-    const user = usersJson.find(({ _id }) => userId === _id);
-    if (!user) {
+  fsPromises
+    .readFile(usersPath, { encoding: 'utf8' })
+    .then((users) => {
+      const usersJson = JSON.parse(users);
+      const user = usersJson.find(({ _id }) => userId === _id);
+      if (!user) {
+        res.status(404);
+        res.send({ message: 'User ID not found' });
+        return;
+      }
+      res.send(user);
+    })
+    .catch(() => {
       res.status(404);
-      res.send({ message: 'User ID not found' });
-      return;
-    }
-    res.send(user);
-  });
+      res.send({ message: 'Requested resource not found' });
+    });
 });
 
 module.exports = router;
